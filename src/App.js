fb1f09ad676913e248f67dc7b7a9d19b5a146354
muggy8 <muggy8@gmail.com>
2019-01-11 05:14:32 -0800

    change the default app.js load in the mapbox element for now

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactMapGL from 'react-map-gl';
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		viewport: {
+			width: 400,
+			height: 400,
+			latitude: 37.7577,
+			longitude: -122.4376,
+			zoom: 8
+		}
+	}
+	render() {
+		return (
+			<div>
+				<ReactMapGL
+				// pass over the relavant attributes in case we add more later
+				width={this.state.viewport.width}
+				height={this.state.viewport.height}
+				latitude={this.state.viewport.latitude}
+				longitude={this.state.viewport.longitude}
+				zoom={this.state.viewport.zoom}
+
+				// handle the onchange event
+				onViewportChange={viewport=>this.setState({viewport})}/>
+			</div>
+		)
+	}
 }
 
 export default App;
