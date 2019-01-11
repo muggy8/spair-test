86c47982ea8425fa3ff2255018f7020f82e20d1c
muggy8 <muggy8@gmail.com>
2019-01-11 10:09:54 -0800

    display all markers in a kinda cute way

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, {Marker} from 'react-map-gl'
 import axios from 'axios'
 // import GtfsRealtimeBindings from 'gtfs-realtime-bindings'
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 const mapBoxToken = "pk.eyJ1IjoibXVnZ3k4IiwiYSI6ImNqcXMyYTk5cTB0Zms0MnA2eG95emFzZDUifQ.dVHK4TIMjLdMvJFUGRm7vQ"
 
 class App extends Component {
@@ -12,9 +12,9 @@ class App extends Component {
 		viewport: {
 			width: 400,
 			height: 400,
-			latitude: 37.7577,
-			longitude: -122.4376,
-			zoom: 8
+			latitude: 49.22160433166661,
+			longitude: -123.06265682276278,
+			zoom: 11
 		},
 		busses: []
 	}
@@ -30,21 +30,42 @@ class App extends Component {
 			this.setState({viewport: newViewport})
 		}
 
+		let isMounted = false
+		const fetchBusData = async()=>{
+			if (!isMounted){
+				return
+			}
+			let markerData = await axios.get("/api/transit-data")
+			this.setState({busses: markerData.data.entity})
+			setTimeout(fetchBusData, 15000) // translink's api updates once ever 30 seconds appearently so we can use this to update it at half the interval so we can get fast data but not waste too many requests
+		}
+
 		this.componentDidMount = ()=>{
+			isMounted = true
 			window.addEventListener("resize", onWindowResize)
 			onWindowResize()
+			fetchBusData()
 		}
 
 		this.componentWillUnmount = ()=>{
+			isMounted = false
 			window.removeEventListener("resize", onWindowResize)
 		}
 
-		// fetch the data from the translink api
-		// this.translinkApi.get("gtfsposition").then(console.log)
+		// fetch the data from the translink api and constructors cant be async sadly
+
+
 	}
 	render() {
+		let busMarkers = this.state.busses.map(bus=>{
+			return (
+				<Marker key={bus.id} longitude={bus.vehicle.position.longitude} latitude={bus.vehicle.position.latitude}>
+				</Marker>
+			)
+		})
+
 		return (
-			<div>
+			<div >
 				<ReactMapGL
 				// pass over the relavant attributes in case we add more later
 				width={this.state.viewport.width}
@@ -55,7 +76,9 @@ class App extends Component {
 				mapboxApiAccessToken={mapBoxToken}
 
 				// handle the onchange event
-				onViewportChange={viewport=>this.setState({viewport})}/>
+				onViewportChange={viewport=>this.setState({viewport})}>
+					{busMarkers}
+				</ReactMapGL>
 			</div>
 		)
 	}
